ce8d01f937718821a886ffbd2fbbedf93d9f948a
Rayna <raynainca190217@gmail.com>
2024-06-06 15:38:20 -0700

    section 2

---
+++ section2-puzzle/section2-puzzle.py
@@ -0,0 +1,16 @@
+import os
+
+SCREENSIZE = (640, 640)
+
+PICTURE_ROOT_DIR = os.path.join(os.getcwd(),'section2/image')
+
+FONT = os.path.join(os.getcwd(),'section2/font/HelloKetta.ttf')
+
+BACKGROUNDCOLO = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+
+RANDOM = 10
